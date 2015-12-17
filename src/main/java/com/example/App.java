ffb651c8a9943f0aefd427cee64d0382bd08c103
Katsumi TAKUSHIMA <katsumit@gold.ocn.ne.jp>
2015-12-17 18:13:07 +0900

    2.1.5 「CommandLineRunner」の利用

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,12 +13,25 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @EnableAutoConfiguration
 @ComponentScan
-public class App {
-	public static void main(String[] args) {
-		try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-			Frontend frontend = context.getBean(Frontend.class);
-			frontend.run();
-		}
+public class App implements CommandLineRunner {
+	
+	@Autowired
+	ArgumentResolver argumentResolver;
 
+	@Autowired
+	Calculator calculator;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println("Enter 2 numbers like 'a b' : ");
+		Argument argument = argumentResolver.resolve(System.in);
+		int result = calculator.calc(argument.getA(), argument.getB());
+		
+		System.out.println("result = " + result);
 	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
 }
