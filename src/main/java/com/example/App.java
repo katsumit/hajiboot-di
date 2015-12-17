1d43a862836aca1c7e3ebdf0c99867aab88e03af
Katsumi TAKUSHIMA <katsumit@gold.ocn.ne.jp>
2015-12-17 18:21:59 +0900

    未使用importの削除

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.example.app.Argument;
